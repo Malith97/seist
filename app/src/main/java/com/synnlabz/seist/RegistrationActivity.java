75429e4d70d42a8a8e64f25f849275eb0e16f51d
Malith <40682828+Malith97@users.noreply.github.com>
2020-02-10 20:57:01 +0530

    UI Stage 1
    Removed ChooseRegistration Page. Now it's redirect into login page and it has option to register. All the other functions are working

--- app/src/main/java/com/synnlabz/seist/RegistrationActivity.java
+++ app/src/main/java/com/synnlabz/seist/RegistrationActivity.java
@@ -5,11 +5,13 @@ import android.content.Intent;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -25,6 +27,17 @@ import java.util.Map;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    RelativeLayout rellay1, rellay2;
+
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            rellay1.setVisibility(View.VISIBLE);
+            rellay2.setVisibility(View.VISIBLE);
+        }
+    };
+
     private Button mRegister;
     private EditText mEmail, mPassword, mName;
 
@@ -37,6 +50,11 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
+        rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
+
+        handler.postDelayed(runnable, 2000); //2000 is the timeout for the splash
 
         mAuth = FirebaseAuth.getInstance();
         firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
@@ -105,5 +123,9 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         mAuth.removeAuthStateListener(firebaseAuthStateListener);
+    }
+
+    public void OpensignUp(View view) {
+        startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
     }
 }
