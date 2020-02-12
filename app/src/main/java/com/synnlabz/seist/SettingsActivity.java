5487c41164382ee82097caa2ac5badcc689acf78
Malith <40682828+Malith97@users.noreply.github.com>
2020-02-12 10:45:00 +0530

    Main Activity Changed
    Remove signout button from main activity and moved into profile activity

--- app/src/main/java/com/synnlabz/seist/SettingsActivity.java
+++ app/src/main/java/com/synnlabz/seist/SettingsActivity.java
@@ -195,4 +195,12 @@ public class SettingsActivity extends AppCompatActivity {
             mProfileImage.setImageURI(resultUri);
         }
     }
+
+    public void logoutUser(View view) {
+        mAuth.signOut();
+        Intent intent = new Intent(SettingsActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+        return;
+    }
 }
